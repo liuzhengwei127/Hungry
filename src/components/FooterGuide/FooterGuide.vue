d7e44a98b4aafdcc2901445ca881d343913b0e71
liuzhengwei127 <756670011@qq.com>
2019-01-25 20:52:33 +0800

    add icons from ali-iconfont

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -8,19 +8,19 @@
       </a>
       <a class="guide_item" :class="{on: '/search' === $route.path}" @click="goTo('/search')">
         <span class="item_icon">
-          <i class="iconfont icon-search"></i>
+          <i class="iconfont icon-ico_search"></i>
         </span>
         <span>搜索</span>
       </a>
       <a class="guide_item" :class="{on: '/order' === $route.path}" @click="goTo('/order')">
         <span class="item_icon">
-          <i class="iconfont iconfont-dingdan"></i>
+          <i class="iconfont icon-dingdan"></i>
         </span>
         <span>订单</span>
       </a>
       <a class="guide_item" :class="{on: '/profile' === $route.path}" @click="goTo('/profile')">
         <span class="item_icon">
-          <i class="iconfont icon-waimai"></i>
+          <i class="iconfont icon-wode"></i>
         </span>
         <span>我的</span>
       </a>
