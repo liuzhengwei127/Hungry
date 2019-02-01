d7bc179fb6238d39bb35d43f68999a7dd266fce1
liuzhengwei127 <756670011@qq.com>
2019-02-01 17:10:50 +0800

    confirm right phone numbers

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -12,8 +12,8 @@
         <form>
           <div :class="{on: loginWay}" @click="loginWay=true">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button disabled="disabled" class="get_verification" :class="{ right_phone: rightPhone}">获取验证码</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -58,6 +58,12 @@ export default {
   data () {
     return {
       loginWay: true, // true代表短信登录，false代表密码登录
+      phone: '' // 手机号
+    }
+  },
+  computed: {
+    rightPhone () {
+      return /^1\d{10}$/.test(this.phone)
     }
   }
 }
@@ -124,6 +130,8 @@ export default {
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone
+                  color black
             .login_verification
               position relative
               margin-top 16px
