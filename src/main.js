c57dd6132f49a08503b957b042b6523d343049bf
liuzhengwei127 <756670011@qq.com>
2019-02-10 09:43:12 +0800

    store login state

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {Button} from 'mint-ui'
+
+Vue.component(Button.name, Button) // <mt-button>
 
 new Vue({
   el: '#app',
--- src/store/actions.js
+++ src/store/actions.js
@@ -9,7 +9,8 @@ import {
 import {
   reqAddress,
   reqFoodCategorys,
-  reqShops
+  reqShops,
+  reqUserInfo
 } from '../api'
 
 export default {
@@ -50,5 +51,14 @@ export default {
   // 同步记录用户信息
   recordUser ({commit}, userInfo) {
     commit(RECEIVE_USER_INFO, {userInfo})
+  },
+
+  // 异步获取用户信息
+  async getUserInfo ({commit}) {
+    const result = await reqUserInfo()
+    if (result.code === 0) {
+      const userInfo = result.data
+      commit(RECEIVE_USER_INFO, {userInfo})
+    }
   }
 }
