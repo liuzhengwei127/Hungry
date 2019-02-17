cbcbc3003ab2602f01cef702e04f60b5b459c52a
liuzhengwei127 <756670011@qq.com>
2019-02-17 16:01:58 +0800

    Search route

--- src/store/actions.js
+++ src/store/actions.js
@@ -10,7 +10,8 @@ import {
   RECEIVE_INFO,
   INCREMENT_FOOD_COUNT,
   DECREMENT_FOOD_COUNT,
-  CLEAR_CART
+  CLEAR_CART,
+  RECEIVE_SEARCH_SHOPS
 } from './mutation-types'
 
 import {
@@ -22,6 +23,7 @@ import {
   reqShopRatings,
   reqShopGoods,
   reqShopInfo,
+  reqSearchShop
 } from '../api'
 
 export default {
@@ -124,5 +126,16 @@ export default {
   // 同步清空购物车
   clearCart ({commit}) {
     commit(CLEAR_CART)
+  },
+
+  // 异步获取商家商品列表
+  async searchShops ({commit, state}, keyword) {
+
+    const geohash = state.latitude + ',' + state.longitude
+    const result = await reqSearchShop(geohash, keyword)
+    if (result.code === 0) {
+      const searchShops = result.data
+      commit(RECEIVE_SEARCH_SHOPS, {searchShops})
+    }
   },
 }
