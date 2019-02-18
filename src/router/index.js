2ad34dcaaac58ace39c466f373707f0997ec71e9
liuzhengwei127 <756670011@qq.com>
2019-02-18 12:30:21 +0800

    lazy load routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,17 @@
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../pages/Home/Home.vue'
-import Search from '../pages/Search/Search.vue'
-import Order from '../pages/Order/Order.vue'
-import Profile from '../pages/Profile/Profile.vue'
+
+// import Home from '../pages/Home/Home.vue'
+// import Search from '../pages/Search/Search.vue'
+// import Order from '../pages/Order/Order.vue'
+// import Profile from '../pages/Profile/Profile.vue'
+
+const Home = () => import('../pages/Home/Home.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
+
 import Login from '../pages/Login/Login.vue'
 import Shop from '../pages/Shop/Shop.vue'
 import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
