844f2f5984d15011e303ebc3747e3dbd330f16ed
liuzhengwei127 <756670011@qq.com>
2019-02-18 14:26:45 +0800

    imgs lazyload

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import {Button} from 'mint-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyload from 'vue-lazyload'
+import loading from './common/images/loading.gif'
 
 import './mock/mockServer'
 
 Vue.component(Button.name, Button) // <mt-button>
+Vue.use(VueLazyload, {
+  loading
+})
 
 new Vue({
   el: '#app',
